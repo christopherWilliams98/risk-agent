44b8c9e08fbfe3b3ebf7a0e9a004a8e83473703b
Christopher Williams <79861789+christopherWilliams98@users.noreply.github.com>
2024-12-13 15:24:55 +0100

    Add files via upload

--- player/player.go
+++ player/player.go
@@ -1,8 +1,6 @@
 package player
 
 import (
-	"fmt"
-	"math/rand"
 	"risk/communication"
 	"risk/game"
 )
@@ -22,72 +20,52 @@ func NewPlayer(id int, comm communication.Communicator) *Player {
 	}
 }
 
-// Play starts the player's turn loop.
-func (p *Player) Play() {
-	for {
-
-		p.SyncGameState()
-
-		action := p.TakeTurn()
-
-		if action.Type == -1 {
-			fmt.Printf("Player %d has no possible actions. Ending turn.\n", p.ID)
-			return
-		}
-
-		action.PlayerID = p.ID
-		p.Communicator.SendAction(action)
-	}
-}
-
-// update the player's local game state.
 func (p *Player) SyncGameState() {
 	p.LocalGameState = p.Communicator.GetGameState()
 }
 
-// decides on an action to perform.
-func (p *Player) TakeTurn() communication.Action {
-	possibleActions := p.generatePossibleActions()
-	if len(possibleActions) == 0 {
-		return communication.Action{Type: -1} // No possible actions
-	}
+// Play starts the player's turn loop.
+func (p *Player) Play() {
+	for {
+		p.SyncGameState()
 
-	// Placeholder TODO right now its just random.
-	chosenAction := possibleActions[rand.Intn(len(possibleActions))]
-	return chosenAction
+		// action := p.TakeTurn()
+
+		// if action.Type == -1 {
+		// 	fmt.Printf("Player %d has no possible actions. Ending turn.\n", p.ID)
+		// 	return
+		// }
+
+		// action.PlayerID = p.ID
+		// p.Communicator.SendAction(action)
+	}
 }
 
-// TODO implement the changes with rules.
-// generatePossibleActions generates all possible actions for the player.
-func (p *Player) generatePossibleActions() []communication.Action {
-	var actions []communication.Action
-	gs := p.LocalGameState
+// update the player's local game state.
+// func (p *Player) TakeTurn() game.Action {
+// 	p.SyncGameState()
+// 	gs := p.LocalGameState
 
-	for cantonID, owner := range gs.Ownership {
-		if owner == p.ID {
-			// Move actions
-			for _, adjID := range gs.Map.Cantons[cantonID].AdjacentIDs {
-				if gs.Ownership[adjID] == p.ID && gs.TroopCounts[cantonID] > 1 {
-					actions = append(actions, communication.Action{
-						Type:         communication.MoveAction,
-						FromCantonID: cantonID,
-						ToCantonID:   adjID,
-						NumTroops:    gs.TroopCounts[cantonID] - 1,
-					})
-				}
-			}
-			// Attack actions
-			for _, adjID := range gs.Map.Cantons[cantonID].AdjacentIDs {
-				if gs.Ownership[adjID] != p.ID && gs.TroopCounts[cantonID] > 1 {
-					actions = append(actions, communication.Action{
-						Type:         communication.AttackAction,
-						FromCantonID: cantonID,
-						ToCantonID:   adjID,
-						NumTroops:    gs.TroopCounts[cantonID] - 1,
-					})
-				}
-			}
-		}
-	}
-	return actions
-}
+// 	// Use MCTS to find the best move
+// 	// mcts := &searcher.UCT{}
+// 	// bestMove := mcts.FindNextMove(gs)
+
+// 	if bestMove == nil {
+// 		possibleMoves := gs.LegalMoves()
+// 		if len(possibleMoves) == 0 {
+// 			return game.Action{Type: PassAction}
+// 		}
+// 		bestMove = possibleMoves[rand.Intn(len(possibleMoves))]
+// 	}
+
+// 	// Convert GameMove to communication.Action
+// 	gameMove := bestMove.(*game.GameMove)
+// 	action := game.Action{
+// 		PlayerID:     p.ID,
+// 		Type:         gameMove.ActionType,
+// 		FromCantonID: gameMove.FromCantonID,
+// 		ToCantonID:   gameMove.ToCantonID,
+// 		NumTroops:    gameMove.NumTroops,
+// 	}
+// 	return action
+// }
